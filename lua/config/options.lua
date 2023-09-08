5e094f1240f4ddb2a828bf80523b3cbfd9bfebfb
Lee <lee10202013@gmail.com>
2023-09-08 13:45:23 +0800

    Disable autoformat

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,10 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+local set = vim.opt -- set options
+set.tabstop = 2
+set.shiftwidth = 2
+set.cindent = true
+set.expandtab = true
+set.number = true
+set.autoindent = false
