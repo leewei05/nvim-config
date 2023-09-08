637f3e58766c489669f89654c0a75ec81ed8e313
Lee <lee10202013@gmail.com>
2023-09-08 18:57:28 +0800

    Update move line mappings

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,1 +1,9 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+local k = vim.keymap
+
+k.set("n", "∆", "<cmd>m .+1<cr>==", { desc = "Move down" })
+k.set("n", "˚", "<cmd>m .-2<cr>==", { desc = "Move up" })
+k.set("i", "∆", "<esc><cmd>m .+1<cr>==gi", { desc = "Move down" })
+k.set("i", "˚", "<esc><cmd>m .-2<cr>==gi", { desc = "Move up" })
+k.set("v", "∆", ":m '>+1<cr>gv=gv", { desc = "Move down" })
+k.set("v", "˚", ":m '<-2<cr>gv=gv", { desc = "Move up" })
