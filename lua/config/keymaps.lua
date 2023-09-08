16b6364e0e5e567725f3c7181aaa87ce3667d362
Lee <lee10202013@gmail.com>
2023-09-08 12:48:42 +0800

    Initial commit

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,8 @@
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+local keymap = vim.keymap
+
+-- Switch windows
+keymap.set("n", "<left>", "<C-W>h")
+keymap.set("n", "<Right>", "<C-W>l")
+keymap.set("n", "<Up>", "<C-W>k")
+keymap.set("n", "<Down>", "<C-W>j")
